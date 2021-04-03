af43be2c9bf2c5f1635f515ca7dece2629d8363b
AlexTutberidze <sandrotutberidze18@gmail.com>
2021-04-03 16:28:54 +0400

    Manga downloader added

--- scraper.py
+++ scraper.py
@@ -1,4 +1,8 @@
 import requests
+import json
+
+chapter = input("Enter chapter to download: ")
+path = input("Enter path were to store downloaded files: ")
 
 headers = {
     'authority': 'attackontitanread.com',
@@ -9,20 +13,26 @@ headers = {
     'sec-fetch-mode': 'navigate',
     'sec-fetch-user': '?1',
     'sec-fetch-dest': 'document',
-    'referer': 'https://attackontitanread.com/manga/attack-on-titan-chapter-129-online-read/',
+    'referer': 'https://attackontitanread.com/manga/attack-on-titan-chapter-[chapter]-online-read/',
     'accept-language': 'en-US,en;q=0.9,ka;q=0.8',
     'cookie': '__cfduid=d37d007a15808a7143c63064f5d961a451617389633',
 }
 
+headers = json.dumps(headers)
+
+headers = headers.replace('[chapter]', chapter)
+
+headers = json.loads(headers)
+
 response = requests.get(
-    'https://attackontitanread.com/manga/attack-on-titan-chapter-130-online-read/', headers=headers)
+    'https://attackontitanread.com/manga/attack-on-titan-chapter-' + chapter + '-online-read/', headers=headers)
 
 
-print(str(response.content).count("<img src=")/2) #count number of images
+print(str(response.content).count("<img src=")) #count number of images
 
-number_of_images = str(response.content).count("<img src=")/2
+number_of_images = str(response.content).count("<img src=")
 
-headers = {
+headers2 = {
     'authority': 'read.mangadad.com',
     'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36',
     'accept': 'image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8',
@@ -33,9 +43,9 @@ headers = {
     'accept-language': 'en-US,en;q=0.9,ka;q=0.8',
 }
 
-response2 = requests.get(
-    'https://read.mangadad.com/Mangadad/attack-on-titan/chapter-130/1.jpg', headers=headers)
+req_str = 'https://read.mangadad.com/Mangadad/attack-on-titan/chapter-130/[count].jpg' #replacement string
 
-
-# for i in range (1, number_of_images + 1):
-
+for i in range (1, int(number_of_images) + 1): #Downloading images
+    x = req_str.replace('[count]', str(i))
+    response2 = requests.get(x, headers=headers2, allow_redirects=True)
+    open(path + str(i) + '.jpg', 'wb').write(response2.content)
